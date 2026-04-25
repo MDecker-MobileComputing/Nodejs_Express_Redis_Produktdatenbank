3e826ca9a27885b662ffce55cedc2306b2acdbda
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-04-25 12:39:38 +0200

    Projekt aufgesetzt

---
+++ README.md
@@ -0,0 +1,19 @@
+# Nodejs mit Redis #
+
+<br>
+
+Die Repo enthält eine einfache Nodejs-Anwendung mit Express.js, die die Verwendung der In-Memory-Datenbank 
+[Redis](https://redis.io/de/) demonstriert.
+
+<br>
+
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License)
+for the files in this repository.
+
+<br>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import express         from "express";
+import expressNunjucks from "express-nunjucks";
+import createLogger    from "logging";
+
+const logger = createLogger( "main" );
+
+
+const PORTNUMMER = process.env.PORTNUMMER || 8000;
+if ( isNaN( PORTNUMMER ) ) {
+
+  logger.error( `FEHLER: Portnummer "${PORTNUMMER}" ist keine Zahl, Abbruch!` );
+  console.log();
+  process.exit( 1 );
+}
+
+
+const expressObjekt = express();
+expressObjekt.use( express.static( "public_html" ) );
+//routenRegistrieren( expressObjekt );
+
+expressObjekt.set( "views" , "templates/" );
+expressNunjucks( expressObjekt, { watch: true, noCache: true }); // bei jedem Request neu laden, damit Änderungen sofort sichtbar sind
+
+expressObjekt.listen( PORTNUMMER,
+                      () => { logger.info(`Web-Server auf Port ${PORTNUMMER} gestartet.\n`); }
+                    );
+                    
