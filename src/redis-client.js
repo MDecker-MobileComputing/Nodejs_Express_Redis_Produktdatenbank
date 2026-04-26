95e5beadfac13ac9bbe3e716122f54a5deb75c22
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-04-26 12:26:36 +0200

    Erste Version der Methode getAllProduktaufrufe()

--- src/redis-client.js
+++ src/redis-client.js
@@ -53,3 +53,38 @@ export async function inkrementProduktNichtGefundenZaehler() {
 
   return redisClient.incr( "produktNichtGefunden" );
 }
+
+
+/**
+ * Alle Zählerwerte für Produktaufrufe abrufen. Hierzu werden alle Keys, die mit
+ * `"produktaufrufe:` beginnen, abgefragt und die zugehörigen Werte ausgelesen.
+ * 
+ * @returns {Promise<Object>} Objekt mit Key-Value-Paaren aller Produktaufrufe.
+ *                            Beispiel, wenn es nur Produkte mit den Nummern `111` und `222` gibt:
+ *                            `{ "111": 42, "222": 7 }`;
+ *                            Objekt kann leer sein (aber nicht `null`), wenn es noch überhaupt
+ *                            keine Produktaufrufe gibt.
+ */
+export async function getAllProduktaufrufe() {
+  
+  const schluesselArray = 
+      await redisClient.keys( "produktaufrufe:*" );
+  
+  if ( schluesselArray.length === 0 ) {
+
+    return {};
+  }
+  
+  const werteArray = 
+      await redisClient.mGet( schluesselArray );  
+  
+  const ergebnisObjekt = {};
+  for ( let i = 0; i < schluesselArray.length; i++ ) {
+
+    const key  = schluesselArray[i].split( ":" )[ 1 ];
+    const wert = werteArray[i];
+    ergebnisObjekt[ key ] = parseInt( wert );
+  }
+  
+  return ergebnisObjekt;
+}
