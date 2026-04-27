8d64a38e8de3a2f328edc4e7e367d06bda309de6
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-04-27 08:44:20 +0200

    redis-client.js: Error Handling vereinfacht

--- src/redis-client.js
+++ src/redis-client.js
@@ -9,33 +9,26 @@ let redisClient = null;
 
 
 /**
- * Verbindung zu Redis-Server aufbauen.  
+ * Verbindung zu Redis-Server aufbauen.
  */
 export async function initRedisClient() {
 
   logger.info( "Initialisierung Redis-Client ..." );
 
-  redisClient 
-          = await createClient().
-                    on( "error",        (fehler) => logger.info(( `Fehler bei Aufbau Verbindung zu Redis-Server: ${fehler}`) ).
-                    on( "connect",      () => logger.info( "Verbindung zum Redis-Server wird aufgebaut ..." ) ). // funktioniert nicht?
-                    on( "ready",        () => logger.info( "Verbindung zum Redis-Server hergestellt." ) ).       // funktioniert nicht?
-                    on( "reconnecting", () => logger.info( "Verbindung zum Redis-Server wird wiederhergestellt ..." ) ).
-                    on( "end",          () => logger.info( "Verbindung zum Redis-Server wurde geschlossen." ) ) ).
-                    connect();
+  redisClient = await createClient().connect();
 }
 
 
 /**
  * Inkrementiert den Page-Impression-Zähler für ein spezifisches Produkt.
- * 
- * @param {number} produktNr Nummer des Produkts, für das der Page-Impression-Zähler 
+ *
+ * @param {number} produktNr Nummer des Produkts, für das der Page-Impression-Zähler
  *                           inkrementiert werden soll
- *  
- * @returns {Promise<number>} Aktueller Wert des Page-Impression-Zählers nach Inkrementierung 
+ *
+ * @returns {Promise<number>} Aktueller Wert des Page-Impression-Zählers nach Inkrementierung
  */
 export async function inkrementPageImpressionZaehler( produktNr ) {
-  
+
   const zaehlerName = `produktaufrufe:${produktNr}`;
   return await redisClient.incr( zaehlerName );
 }
@@ -43,8 +36,8 @@ export async function inkrementPageImpressionZaehler( produktNr ) {
 
 /**
  * Inkrementiert den "Produkt nicht gefunden"-Zähler.
- * 
- * @returns {Promise<number>} Aktueller Wert des "Produkt nicht gefunden"-Zählers 
+ *
+ * @returns {Promise<number>} Aktueller Wert des "Produkt nicht gefunden"-Zählers
  *                            nach Inkrementierung
  */
 export async function inkrementProduktNichtGefundenZaehler() {
@@ -56,7 +49,7 @@ export async function inkrementProduktNichtGefundenZaehler() {
 /**
  * Alle Zählerwerte für Produktaufrufe abrufen. Hierzu werden alle Keys, die mit
  * `"produktaufrufe:` beginnen, abgefragt und die zugehörigen Werte ausgelesen.
- * 
+ *
  * @returns {Promise<Array>} 2-dimensionaler Array aller Produktaufrufe.
  *                           Jedes Element ist ein Array mit zwei Komponenten:
  *                           Index 0: Produktnummer (String), Index 1: Anzahl Aufrufe (Number).
@@ -65,18 +58,18 @@ export async function inkrementProduktNichtGefundenZaehler() {
  *                           Array kann leer sein, wenn es noch überhaupt keine Produktaufrufe gibt.
  */
 export async function getAllProduktaufrufe() {
-  
-  const schluesselArray = 
+
+  const schluesselArray =
       await redisClient.keys( "produktaufrufe:*" );
-  
+
   if ( schluesselArray.length === 0 ) {
 
     return [];
   }
-  
-  const werteArray = 
-      await redisClient.mGet( schluesselArray );  
-  
+
+  const werteArray =
+      await redisClient.mGet( schluesselArray );
+
   const ergebnisArray = [];
   for ( let i = 0; i < schluesselArray.length; i++ ) {
 
@@ -90,20 +83,20 @@ export async function getAllProduktaufrufe() {
     const zweierArray = [ produktNrNumber, anzahlAufrufNumber ];
     ergebnisArray.push( zweierArray );
   }
-  
+
  // Array nach Zahl in erster Komponente sortieren, d.h. nach Produktnummern aufsteigend sortieren
   ergebnisArray.sort( ( a, b ) => a[0] - b[0] );
 
   return ergebnisArray;
 }
- 
+
 
 /**
  * Ruft den Wechselkurs für eine spezifische Fremdwährung aus dem Cache ab.
- * 
+ *
  * @param {string} fremdwaehrung, z.B. "USD" oder "GBP", muss auf Großbuchstaben normiert sein
- * 
- * @returns Wechselkurs von Euro in angegebene Fremdwährung (cache hit) oder `NaN`, wenn kein Wechselkurs 
+ *
+ * @returns Wechselkurs von Euro in angegebene Fremdwährung (cache hit) oder `NaN`, wenn kein Wechselkurs
  *          im Cache gefunden wurde (cache miss)
  */
 export async function getWechselkurs( fremdwaehrung ) {
@@ -117,11 +110,11 @@ export async function getWechselkurs( fremdwaehrung ) {
  * Speichert den Wechselkurs für eine spezifische Fremdwährung im Cache.
  * Der Wert wird 24 Stunden lang im Cache gehalten, danach verfällt er automatisch, weil
  * die Frankfurter-API nur tagesaktuelle Wechselkurse liefert.
- * 
+ *
  * @param {string} fremdwaehrung, z.B. "USD" oder "GBP", muss auf Großbuchstaben normiert sein
- * 
+ *
  * @param {number} wechselkurs Wechselkurs von Euro in die angegebene Fremdwährung
- * 
+ *
  * @returns {Promise<boolean>} `true`, wenn der Wechselkurs erfolgreich im Cache gespeichert wurde, sonst `false`
  */
 export async function setWechselkurs( fremdwaehrung, wechselkurs ) {
@@ -129,9 +122,9 @@ export async function setWechselkurs( fremdwaehrung, wechselkurs ) {
   const cacheKey = `umrechnungskurs:${fremdwaehrung}`;
 
   const cacheKonfig = { EX: 24*3600 }; // EX: Expire time in seconds, hier: 24 Stunden
-                            
-  return await redisClient.set( 
-                          cacheKey, 
-                          wechselkurs.toString(), 
+
+  return await redisClient.set(
+                          cacheKey,
+                          wechselkurs.toString(),
                           cacheKonfig );
 }
